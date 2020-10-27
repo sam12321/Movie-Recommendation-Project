e7f19d7d0a596c7605e754be4d4b0b0cd1ceda1d
Bhavya <me@example.com>
2020-10-27 18:46:24 +0530

    Movie Recommendation Pro

---
+++ MovieRecommendationPro.scala
@@ -0,0 +1,42 @@
+import org.apache.spark._
+import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{FloatType, IntegerType, LongType, StringType, StructType}
+import org.sparkproject.dmg.pmml.True
+
+object MovieRecommendationPro{
+
+  // case class for movie dataset
+  case class Movies(movieId: Int, movieTitle: String)
+
+  // case class for ratings
+  case class Ratings(userID: Int, movieID: Int, rating: Float)
+
+  def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark =SparkSession
+      .builder()
+      .appName("MovieRecommendationPro")
+      .master("local[*]")
+      .getOrCreate()
+
+    val schemaMovie = new StructType()
+      .add("movieId",IntegerType,nullable = true)
+      .add("movieTitle", StringType, nullable = true)
+
+    val ratingSchema = new StructType()
+      .add("userID",IntegerType,nullable = true)
+      .add("movieId",IntegerType,nullable = true)
+      .add("rating",FloatType,nullable = true)
+      .add("timeStamp",LongType,nullable = true)
+
+    import spark.implicits._
+
+
+
+
+  }
+
+}
